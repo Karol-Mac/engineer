4f9f0883e5012f64bea46456c3fc9c728b0fe7ee
Karol Mac <karol200242@gmail.com>
2024-07-14 20:42:59 +0200

    added endpoints to SecurityConfig

--- src/main/java/com/example/engineer/config/SecurityConfig.java
+++ src/main/java/com/example/engineer/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.engineer.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,9 +26,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers( "/api/auth/**").permitAll()
+                                .requestMatchers( HttpMethod.GET, "/api/products/**").permitAll()
+                                .requestMatchers( HttpMethod.GET, "/api/images").permitAll()
+                                .requestMatchers( HttpMethod.GET, "/api/comments").permitAll()
                                 .anyRequest().authenticated()
                 )
-//                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
