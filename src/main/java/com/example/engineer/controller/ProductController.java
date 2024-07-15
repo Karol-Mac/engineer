fa9654a5f8fab680ba4398aecc7d7522b625c1e6
Karol Mac <karol200242@gmail.com>
2024-07-15 17:33:17 +0200

    add role detection in product's operation

--- src/main/java/com/example/engineer/controller/ProductController.java
+++ src/main/java/com/example/engineer/controller/ProductController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,15 +27,13 @@ public class ProductController {
         this.imageService = imageService;
     }
 
+    @PreAuthorize("hasRole('SELLER')")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<FreshProductDto> addProduct(@RequestPart("product") @Valid FreshProductDto product,
                                                       @RequestPart(value = "file")  MultipartFile imageFile)
                                                         throws IOException{
 
-        String imageName = imageService.saveImage(imageFile);
-        product.setImageName(imageName);
-
-        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
+        return new ResponseEntity<>(productService.addProduct(product, imageFile), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
@@ -42,19 +41,20 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
-    //TODO: add rest of controller methods
     @GetMapping
     public ResponseEntity<List<ProductDto>> getProductsByName(@RequestParam String name){
         return ResponseEntity.ok(productService.getAllProducts(name));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('SELLER, ADMIN')")
     public ResponseEntity<FreshProductDto> updateProduct(@PathVariable long id,
                                                          @RequestBody @Valid FreshProductDto product){
         return ResponseEntity.ok(productService.updateProduct(product, id));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('SELLER, ADMIN')")
     public ResponseEntity<String> deleteProduct(@PathVariable long id){
         return ResponseEntity.ok(productService.deleteProduct(id));
     }
