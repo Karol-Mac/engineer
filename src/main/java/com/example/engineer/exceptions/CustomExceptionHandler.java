2dd8c83f10e926d189f428d4d95f5f7e88311927
Karol Mac <karol200242@gmail.com>
2024-07-16 09:12:41 +0200

    So...I override handleExceptionInternal method - it is called for every exception (in parent class - ResponseEntityExceptionHandler). For know everything work's as I wanted to, but I left comment to pin, that It might cause problems in future

--- src/main/java/com/example/engineer/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/example/engineer/exceptions/CustomExceptionHandler.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -25,18 +23,10 @@ import java.util.Map;
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(AccessDeniedException.class)
+    @ExceptionHandler({AccessDeniedException.class, AuthenticationException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ResponseBody
     public ResponseEntity<ErrorInfo> handleAccessDeniedException(AccessDeniedException ex, HttpServletRequest request) {
-        ErrorInfo errorInfo = new ErrorInfo(request.getRequestURI(),  ex);
-        return new ResponseEntity<>(errorInfo, HttpStatus.FORBIDDEN);
-    }
-
-    @ExceptionHandler(AuthenticationException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ResponseBody
-    public ResponseEntity<ErrorInfo> handleAuthenticationException(AuthenticationException ex, HttpServletRequest request) {
         ErrorInfo errorInfo = new ErrorInfo(request.getRequestURI(),  ex);
         return new ResponseEntity<>(errorInfo, HttpStatus.FORBIDDEN);
     }
@@ -56,13 +46,6 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(new ErrorInfo(request.getRequestURI(), ex), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public ResponseEntity<ErrorInfo> handleGlobalException(Exception ex, HttpServletRequest request) {
-        return new ResponseEntity<>(new ErrorInfo(request.getRequestURI(), ex), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
     @Override
     public ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers,
@@ -78,22 +61,12 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errors, status);
     }
 
+    //FIXME: in the future this might cause problems
     @Override
-    public ResponseEntity<Object> handleMaxUploadSizeExceededException(
-            MaxUploadSizeExceededException ex, HttpHeaders headers,
-            HttpStatusCode status, WebRequest request) {
+    public ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,HttpHeaders headers,
+                                                          HttpStatusCode statusCode, WebRequest request){
 
-        var errorInfo = new ErrorInfo(request.getDescription(false) , ex);
-        return new ResponseEntity<>(errorInfo, status);
+        var errorInfo = new ErrorInfo(request.getDescription(false).substring(4) , ex);
+        return new ResponseEntity<>(errorInfo, statusCode);
     }
-
-    @Override
-    public ResponseEntity<Object> handleMissingServletRequestPart(
-            MissingServletRequestPartException ex, HttpHeaders headers,
-            HttpStatusCode status, WebRequest request) {
-
-        var errorInfo = new ErrorInfo(request.getDescription(false) , ex);
-        return new ResponseEntity<>(errorInfo, status);
-    }
-
 }
