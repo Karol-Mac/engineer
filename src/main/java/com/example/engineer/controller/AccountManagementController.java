f8cb3c10b4a7482482d5f955e6b59dd6969764cd
Karol Mac <karol200242@gmail.com>
2024-07-18 18:42:19 +0200

    methods implemented in controller + tested All good :))

--- src/main/java/com/example/engineer/controller/AccountManagementController.java
+++ src/main/java/com/example/engineer/controller/AccountManagementController.java
@@ -3,6 +3,7 @@ package com.example.engineer.controller;
 import com.example.engineer.payload.AccountDto;
 import com.example.engineer.payload.RegisterUserDto;
 import com.example.engineer.service.AccountManagementService;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class AccountManagementController {
         this.accountManagementService = accountManagementService;
     }
 
-    @PutMapping
+    @PostMapping
     @PreAuthorize("hasAnyRole(@userRole, @adminRole)")
     public ResponseEntity<RegisterUserDto> changeCredentials(@RequestBody RegisterUserDto registerUserDto) {
         var username = registerUserDto.getUsername();
@@ -30,11 +31,24 @@ public class AccountManagementController {
 
     @GetMapping
     @PreAuthorize("hasRole(@adminRole)")
-    public ResponseEntity<List<AccountDto>> getAccountsByName(
+    public ResponseEntity<List<AccountDto>> getAccounts(
                             @RequestParam(defaultValue = "") String name) {
 
         return name.isBlank() ?
                 ResponseEntity.ok(accountManagementService.getAllUsers()) :
                 ResponseEntity.ok(accountManagementService.getAccountsByName(name));
+    }
+
+    @PutMapping
+    @PreAuthorize("hasRole(@adminRole)")
+    public ResponseEntity<AccountDto> editAccount(
+            @RequestBody AccountDto accountDto) throws BadRequestException {
+        return ResponseEntity.ok(accountManagementService.updateUser(accountDto));
+    }
+
+    @DeleteMapping("/{userId}/comments")
+    @PreAuthorize("hasRole(@adminRole)")
+    public ResponseEntity<String> deleteUserComments(@PathVariable Long userId) throws BadRequestException {
+        return ResponseEntity.ok(accountManagementService.removeAllComments(userId));
     }
 }
