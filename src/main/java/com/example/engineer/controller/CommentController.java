0c5962aaa3f793c9b29a3f01d430dab3e64300b2
Karol Mac <karol200242@gmail.com>
2024-07-19 08:40:26 +0200

    controller methods creted & tested :)

---
+++ src/main/java/com/example/engineer/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.example.engineer.controller;
+
+import com.example.engineer.payload.CommentDto;
+import com.example.engineer.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> getAllComments(){
+        return ResponseEntity.ok(commentService.getAllComments());
+    }
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    @PreAuthorize("hasRole(@userRole)")
+    public ResponseEntity<CommentDto> createComment(@RequestBody String content){
+        return new ResponseEntity<>(commentService.addComment(content), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{commentId}")
+    @PreAuthorize("hasRole(@adminRole)")
+    public ResponseEntity<CommentDto> getSingleComment(@PathVariable long commentId){
+        return ResponseEntity.ok(commentService.getCommentById(commentId));
+    }
+
+    @DeleteMapping("/{commentId}")
+    @PreAuthorize("hasRole(@adminRole)")
+    public ResponseEntity<String> deleteComment(@PathVariable long commentId){
+        return ResponseEntity.ok(commentService.deleteComment(commentId));
+    }
+}
--- src/main/java/com/example/engineer/util/mappers/CommentMapper.java
+++ src/main/java/com/example/engineer/util/mappers/CommentMapper.java
@@ -11,9 +11,13 @@ public class CommentMapper {
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
         commentDto.setContent(comment.getContent());
-        commentDto.setAuthorName(comment.getUser().getUsername());
+        commentDto.setAuthorName(comment.getUser().getRealUsername());
         commentDto.setIsVisible(comment.getIsVisible());
-        commentDto.setIsReported(!comment.getReports().isEmpty());
+
+        //when new comment is created - report's list is a null,
+        // but when It's retrieved from DB (and doesn't have any reports)
+        // it is an empty list
+        commentDto.setIsReported(!(comment.getReports() == null || comment.getReports().isEmpty()));
 
         return commentDto;
     }
