1beffbfa5c011807a1df8c3387290fadb121b3ab
Karol Mac <karol200242@gmail.com>
2024-07-20 18:31:00 +0200

    All product's are added
    Bug fixed:
    admin's can delete product's too

--- src/main/java/com/example/engineer/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/engineer/service/impl/ProductServiceImpl.java
@@ -7,10 +7,11 @@ import com.example.engineer.payload.FreshProductDto;
 import com.example.engineer.payload.ProductDto;
 import com.example.engineer.repository.ProductRepository;
 import com.example.engineer.repository.SellerRepository;
+import com.example.engineer.repository.UserRepository;
 import com.example.engineer.service.ImageService;
 import com.example.engineer.service.ProductService;
+import com.example.engineer.util.RoleBeans;
 import com.example.engineer.util.mappers.ProductMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -20,13 +21,23 @@ import java.io.IOException;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final SellerRepository sellerRepository;
+    private final UserRepository userRepository;
     private final ProductMapper productMapper;
     private final ImageService imageService;
+
+    public ProductServiceImpl(ProductRepository productRepository, SellerRepository sellerRepository,
+                              UserRepository userRepository, ProductMapper productMapper,
+                              ImageService imageService){
+        this.productRepository = productRepository;
+        this.sellerRepository = sellerRepository;
+        this.userRepository = userRepository;
+        this.productMapper = productMapper;
+        this.imageService = imageService;
+    }
 
 
     @Override
@@ -34,7 +45,7 @@ public class ProductServiceImpl implements ProductService {
 
         Product product = productMapper.mapToEntity(freshProduct);
 
-        Seller owner = getSellerFromDB();
+        Seller owner = getSellerFromSession();
         String imageName = imageService.saveImage(imageFile);
 
         product.setSeller(owner);
@@ -64,6 +75,8 @@ public class ProductServiceImpl implements ProductService {
     public FreshProductDto updateProduct(FreshProductDto freshProductDto, long productId){
         Product actual = getProductFromDB(productId);
 
+        getOwner(actual.getSeller().getId(), getSellerFromSession());
+
         productMapper.copyCommonFields(freshProductDto, actual);
         actual.setId(productId);
 
@@ -73,21 +86,44 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public String deleteProduct(long productId){
         var product = getProductFromDB(productId);
+        var email = getUserEmail();
+        //check if user is admin:
+        var admin = userRepository.findByEmail(email).orElse(null);
+
+        //if no - check if it's a seller
+        if (admin == null || !admin.getRole().getName().contains(RoleBeans.ADMIN)) {
+            getOwner(product.getSeller().getId(), getSellerFromSession());
+        }
+        return deleteProduct(product);
+    }
+
+    private String deleteProduct(Product product){
         product.setIsHidden(true);
         productRepository.save(product);
-
         return "Product deleted succesfully";
     }
+
 
     private Product getProductFromDB(long productId){
         return productRepository.findById(productId)
                 .orElseThrow(()->new NotFoundException(Product.class.getSimpleName(), productId));
     }
 
-    private Seller getSellerFromDB(){
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return sellerRepository.findByEmail(email)
-                .orElseThrow(() -> new AccessDeniedException("You have no permission to access this resource"));
+    private String getUserEmail(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
+    private Seller getSellerFromSession(){
+
+        return sellerRepository.findByEmail(getUserEmail()).orElseThrow(
+                () -> new AccessDeniedException("You have no permission to access this resource"));
+    }
+
+    private void getOwner(long sellerId, Seller loggedIn){
+        var owner = sellerRepository.findById(sellerId).orElseThrow(
+                () -> new NotFoundException("Seller", sellerId));
+
+        if(!owner.equals(loggedIn))
+            throw new AccessDeniedException("You are not the owner of this product");
+    }
 }
