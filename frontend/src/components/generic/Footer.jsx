c0ae4d7078d68c9ad1ac6787ca57446c92163bf8
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-18 23:09:21 +0100

    Fixed Footer and HeaderSimple

--- frontend/src/components/generic/Footer.jsx
+++ frontend/src/components/generic/Footer.jsx
@@ -1,20 +1,19 @@
 import {NavigateFunctions} from "../functions/NavigateFunctions"
 import CompareProductsButton from "./CompareProductsButton";
 
-import "../../css/generalVisuals.css";
+// import "../../css/generalVisuals.css";
+import styles from "../../css/Footer.module.css";
 
 const Footer = () => {
     const {openContactpage} = NavigateFunctions();
     return (
         <div id="footer">
-            <div className="footerContent">
+            <div className={styles.footer}>
                 <p>Got Questions? <span>+48 123 123 133</span></p>
                 <div className="footerLinks">
-                    <h6 onClick={openContactpage} className="footerLink">Contact Us</h6>
-                    <h6 onClick={openContactpage} className="footerLink">Terms of Service</h6>
+                    <h6 onClick={openContactpage} className={styles.footerLink}>Contact Us</h6>
+                    <h6 onClick={openContactpage} className={styles.footerLink}>Terms of Service</h6>
                 </div>
-
-            {/*<CompareProductsButton givenProductID={11}/>/!* for tests *!/*/}
             </div>
         </div>
     );
--- frontend/src/components/generic/HeaderSimple.jsx
+++ frontend/src/components/generic/HeaderSimple.jsx
@@ -3,8 +3,11 @@
 import {useState} from "react";
 import {NavigateFunctions} from "../functions/NavigateFunctions" //not imported as default therefore it has to be in curly brackets
 import SideMenu from "../specific/loginpage/SideMenu";
-import "../../css/generalVisuals.css";
+// import "../../css/generalVisuals.css";
+import "../../css/HeaderSimple.module.css";
+
 import RollbackPageButton from "./RollbackPageButton";
+import styles from "../../css/HeaderSimple.module.css";
 
 
 const HeaderSimple = () => { //pass Token if token is null than display login + Register else display
@@ -12,11 +15,10 @@ const HeaderSimple = () => { //pass Token if token is null than display login + 
 
 
     return(
-        <div id="header">
+        <div className={styles.header}>
             <img src="/images/icons/HeaderLogoImg.png" alt="HeaderLogoImg.png" id="HeaderLogoImg" onClick={openHomepage}/>
-
-
         </div>
+
     );
 };
 
