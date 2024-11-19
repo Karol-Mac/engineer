200be78c8f0f7cbdf66d461a472bade7c3209c3f
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-19 16:47:17 +0100

    Different compare toolbar

--- frontend/src/components/generic/ProductCompareToolbar.jsx
+++ frontend/src/components/generic/ProductCompareToolbar.jsx
@@ -1,11 +1,14 @@
 import {CompareFunctions} from "../functions/CompareFunctions";
 import {useEffect, useState} from "react";
 import {CustomEventsControler} from "../functions/CustomEventsControler";
+import styles from "../../css/ProductCompareToolbar.module.css";
+import {NavigateFunctions} from "../functions/NavigateFunctions";
 
 const ProductCompareToolbar = () => {
     const {isAnyProductToCompareSelected,initializeProductComparisonList} = CompareFunctions();
     const {addListenerDispatchOnCompareUpdate,removeListenerDispatchOnCompareUpdate} = CustomEventsControler();
     const [isProductCompareToolbarVisible, setIsProductCompareToolbarVisible] = useState(false);
+    const {openComparepage} = NavigateFunctions();
 
     initializeProductComparisonList();
 
@@ -23,25 +26,28 @@ const ProductCompareToolbar = () => {
         }
     }, []); //Pozbycie się ", []" usuwa warning ale w konsoli pokazane jest jakby dwukrotnie wywołana była ta funkcja. tak czy inaczej działa
 
+    const handleClick = () => {
+        openComparepage();
+    };
 
-    return(
-        <div>
-        <a id="CompareImg">
-        {isProductCompareToolbarVisible ? (
-                <div>
-                    <p>Compare is visible</p>
-                {/*add images of products to compare + button to the webpage*/}
-                </div>
-            ) : (
-                <div>
-                    <p>Compare is NOT visible</p>
-                </div>
-            )}
-            {/*<img src="CompareLogoImg.jpg" alt="CompareLogoImg.jpg" id="CompareLogoImg2"/>*/}
-        </a>
+    return (
+        <div className={styles.toolbarContainer}>
+            <a id="CompareImg">
+                {isProductCompareToolbarVisible ? (
+                    <div className={styles.toolbarVisible} onClick={handleClick}>
+                        <p className={styles.compareButton}>Compare is visible</p>
+                    </div>
+                ) : (
+                    <div>
+                        {/*<p>Compare is NOT visible</p>*/}
+                    </div>
+                )}
+                {/*<img src="CompareLogoImg.jpg" alt="CompareLogoImg.jpg" id="CompareLogoImg2"/>*/}
+            </a>
         </div>
     );
 };
 
 export default ProductCompareToolbar;
+
 
